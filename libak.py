2f345c4ff55bbb8015ce01980af2ec60874ef0b7
danielgyorgy224 <127303436+danielgyorgy224@users.noreply.github.com>
2024-03-13 11:42:32 +0100

    c1) és c2) feladat megoldva

--- libak.py
+++ libak.py
@@ -26,20 +26,53 @@ def roka_osszeg(l):
     db = 0
     sum = 0
     for suly in l:
-        if suly < 4:
+        if suly <= 3:
             db+=1
             sum+=suly
     return sum, db
 
-def kiir(list, ro, rdb):
+def get_legharom(l):
+    i = 0
+    b = False
+    s = -1
+    while i < len(l) and not l[i] >= 3:
+        i += 1
+    if i < len(l):
+        b = True
+        s = i
+    return b, s
+
+def get_elozonap(l):
+    i = len(l)-1
+    b = False
+    while i>0 and not l[i-1] < l[i]:
+        i -= 1
+    if i > 0:
+        b = True
+    return b
+
+def kiir(list, ro, rdb, l_s, en_s):
     print(f"Libák súlyai (kg): {', '.join(map(str, list))}")
     print(f"a. {ro} kiló libát ehet meg a róka.")
     print(f"b. Átlagosan {ro/rdb:.2f} kilósak a rókának maradt libák.")
+    print(f"c1. {l_s} olyan, hogy a róka legalább háromkilós libát lopott?")
+    print(f"c2. {en_s} olyan, hogy a róka kisebb libát hozott, mint az előző napon?")
 
 # főprogram
 # input
 libak = beolvasas()
 # számítás
 rosszeg, rdarab = roka_osszeg(libak)
+legalabbharom, elso_legharom_i = get_legharom(libak)
+if legalabbharom:
+    legalabb3_s = "Előfordult"
+else:
+    legalabb3_s = "Nem fordult elő"
+elso_legharom = libak[elso_legharom_i]
+elozonap_tobb = get_elozonap(libak)
+if elozonap_tobb:
+    enap_tobb_s = "Előfordult"
+else:
+    enap_tobb_s = "Nem fordult elő"
 # output
-kiir(libak, rosszeg, rdarab)
+kiir(libak, rosszeg, rdarab, legalabb3_s, enap_tobb_s)
